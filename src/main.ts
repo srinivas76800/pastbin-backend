6379b933bb8e9d64fe6bf40d07e09e0aa99c318b
Vemunuri Vivek <srinivasvemunuri8@email.com>
2026-01-29 10:00:32 +0530

    changed port number

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   app.enableCors({
-    origin: 'http://localhost:3001', // frontend URL
+    origin: [
+    'http://localhost:3001',              // local frontend
+    'https://your-frontend-domain.com',   // deployed frontend
+  ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
+      credentials: true,
   });
 
-  await app.listen(3000);
+await app.listen(3000);
 }
 bootstrap();
