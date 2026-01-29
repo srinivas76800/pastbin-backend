868ef9a98eaa4f977ffd9e8c42cf2be0f5e8c12d
Vemunuri Vivek <srinivasvemunuri8@email.com>
2026-01-29 11:45:39 +0530

    added 0000 in port

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   });
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log('🚀 Server running on port:', port);
   console.log('Server running on port:', port);
 }
